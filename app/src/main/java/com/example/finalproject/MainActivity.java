ae82c7b5b9d7dab3a471703f5b22b2e50a057ed7
ashleyeah <ayeah2@illinois.edu>
2019-12-02 20:17:56 -0600

    las

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -23,7 +23,12 @@ public class MainActivity extends AppCompatActivity {
         Button eng = findViewById(R.id.eng);
         Button bus = findViewById(R.id.bus);
         Button aces = findViewById(R.id.aces);
+        Button faa = findViewById(R.id.faa);
         las.setOnClickListener(unused -> startActivity(new Intent(this, LasActivity.class)));
+        eng.setOnClickListener(unused -> startActivity(new Intent(this, EngActivity.class)));
+        bus.setOnClickListener(unused -> startActivity(new Intent(this, BusActivity.class)));
+        aces.setOnClickListener(unused -> startActivity(new Intent(this, OtherActivity.class)));
+        faa.setOnClickListener(unused -> startActivity(new Intent(this, OtherActivity.class)));
 
 
     }
