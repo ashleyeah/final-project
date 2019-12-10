994e95884b4cd7927e34a88c5cb4c39d780726ef
ashleyeah <ayeah2@illinois.edu>
2019-12-10 00:50:52 -0600

    actually done

--- app/src/main/java/com/example/finalproject/WebActivity.java
+++ app/src/main/java/com/example/finalproject/WebActivity.java
@@ -1,13 +1,17 @@
 package com.example.finalproject;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class WebActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +19,7 @@ public class WebActivity extends AppCompatActivity {
 
         WebView page = findViewById(R.id.page);
         page.getSettings().setDomStorageEnabled(true);
+        page.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
         page.loadUrl("https://waf.cs.illinois.edu/discovery/every_gen_ed_at_uiuc_by_gpa/");
     }
 }
